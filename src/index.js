718d1b0d5fbcc0930ba7ab09ff1ddca662beeebe
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2022-09-03 17:44:42 +0000

    Add other

--- src/index.js
+++ src/index.js
@@ -5,49 +5,44 @@ import ClientForm from "./ClientForm";
 
 import "./styles.css";
 
-const App =  () => {
-
+const App = () => {
   const [clients, setNewCilent] = useState([
     { id: 1, nom: "Amadou Benthe" },
     { id: 2, nom: "Nadia ZEBATO" },
-    { id: 3, nom: "Joseph Durand" }
-  ])
+    { id: 3, nom: "Joseph Durand" },
+    { id: 4, nom: "Joseph Durand Autre" },
+  ]);
 
-  const handleDelete = id => {
+  const handleDelete = (id) => {
     const updatetedClients = [...clients];
-    const index = clients.findIndex(client => client.id === id);
+    const index = clients.findIndex((client) => client.id === id);
 
     updatetedClients.splice(index, 1);
 
     setNewCilent(updatetedClients);
   };
 
-  const handleAdd = client => {
+  const handleAdd = (client) => {
     const updatetedClients = [...clients];
     updatetedClients.push(client);
 
     setNewCilent(updatetedClients);
   };
 
+  const title = "Liste des clients";
 
-    const title = "Liste des clients";
-
-    return (
-      <div>
-        <h1>{title}</h1>
-        <ul>
-          {clients.map(client => (
-            <Client
-              key={client.id}
-              details={client}
-              onDelete={handleDelete}
-            />
-          ))}
-        </ul>
-        <ClientForm onClientAdd={handleAdd} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {clients.map((client) => (
+          <Client key={client.id} details={client} onDelete={handleDelete} />
+        ))}
+      </ul>
+      <ClientForm onClientAdd={handleAdd} />
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement); 
+ReactDOM.render(<App />, rootElement);
